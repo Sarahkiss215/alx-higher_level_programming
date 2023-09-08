c2aee9f1d3884c7a6fae6ab2eb1a3073d1e0298a
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-08 09:02:30 -0700

    Add functions to header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,5 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+void reverse(listint_t **h_r);
+int compare(listint_t *h1, listint_t *h2);
 
 #endif /* LISTS_H */
