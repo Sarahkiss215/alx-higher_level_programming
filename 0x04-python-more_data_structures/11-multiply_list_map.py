43edfa18746098890ab5b0609677a27ef4cc1b00
Sarahkiss215 <skisiangani5@gmail.com>
2023-09-13 17:12:20 -0700

    Correct pycodestyle error in line 3

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """ Returns a list with all values multiplied by a number without using any loops """
+    """ Returns a list with all values """
     return (list(map(lambda x: x*number, my_list)))
