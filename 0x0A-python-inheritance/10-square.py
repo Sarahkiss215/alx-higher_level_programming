d88647798836a074468e00cf8ffd1d64bf597b55
Sarahkiss215 <skisiangani5@gmail.com>
2023-10-09 21:46:14 -0700

    Add function that defines a square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines  a class Square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize a new square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
