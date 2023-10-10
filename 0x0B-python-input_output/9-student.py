d3628a1d0be0c5d69f8f7c583de363df4c534c7d
Sarahkiss215 <skisiangani5@gmail.com>
2023-10-10 01:30:22 -0700

    Add a class Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module 9-student.py"""
+
+
+class Student:
+    """Defines a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+         """Initialize a Student"""
+         self.first_name = first_name
+         self.last_name = last_name
+         self.age = age
+
+         def to_json(self):
+             return self.__dict__
