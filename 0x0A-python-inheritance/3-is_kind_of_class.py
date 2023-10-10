677ef20f3e0e338365edff7ba063e0c1baf31b33
Sarahkiss215 <skisiangani5@gmail.com>
2023-10-09 20:45:32 -0700

    Create program that checks if an object is an instance of class inherited from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 3-is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if the object is an instance of a class inherited from"""
+
+    return isinstance(obj, a_class)
