669f24c766e412180886c97cfcd420965a873e33
Sarahkiss215 <skisiangani5@gmail.com>
2023-10-10 01:34:45 -0700

    Correct pycodestyle error

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,10 +6,10 @@ class Student:
     """Defines a class Student"""
 
     def __init__(self, first_name, last_name, age):
-         """Initialize a Student"""
-         self.first_name = first_name
-         self.last_name = last_name
-         self.age = age
+        """Initialize a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-         def to_json(self):
-             return self.__dict__
+    def to_json(self):
+        return self.__dict__
